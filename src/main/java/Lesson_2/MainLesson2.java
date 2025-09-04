6180a8e30a108510314833daf71b36a6cc8035b3
Максим <maksimka.baranov.80@mail.ru>
2025-09-04 21:30:25 +0300

    Add answer

---
+++ src/main/java/Lesson_2/MainLesson2.java
@@ -0,0 +1,15 @@
+package Lesson_2;
+//1.Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple//
+
+public class MainLesson2 {
+
+    public static void printThreeWords() {
+        System.out.println("\nOrange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+}
