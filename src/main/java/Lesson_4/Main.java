6fec6aabc3a9348c018c112e15f35092317ee8ab
Максим <maksimka.baranov.80@mail.ru>
2025-09-10 16:52:19 +0300

    Add answer

--- src/main/java/Lesson_4/Main.java
+++ src/main/java/Lesson_4/Main.java
@@ -165,7 +165,104 @@ public class Main {
     Результат полученных характеристик [ Периметр, площадь, цвет фона, цвет границ ] по каждой фигуре вывести в консоль.
     Попробуйте реализовать базовые методы, такие как расчет периметра фигур, в качестве дефолтных методов в интерфейсе.*/
 
+    interface Figure {
 
+        double getPerimeter();
+        double getArea();
+        String getFillColor();
+        String getBorderColor();
+    }
+
+    static class Circle implements Figure {
+        private double radius;
+        private String fillColor;
+        private String borderColor;
+
+        public Circle(double radius) {
+            this.radius = radius;
+            this.fillColor = "Красный";
+            this.borderColor = "Черный";
+        }
+
+        public double getPerimeter() {
+            return 2 * 3.14 * radius;
+        }
+
+        public double getArea() {
+            return 3.14 * radius * radius;
+        }
+
+        public String getFillColor() {
+            return fillColor;
+        }
+
+        public String getBorderColor() {
+            return borderColor;
+        }
+    }
+
+    static class Rectangle implements Figure {
+        private double width;
+        private double height;
+        private String fillColor;
+        private String borderColor;
+
+        public Rectangle(double width, double height) {
+            this.width = width;
+            this.height = height;
+            this.fillColor = "Синий";
+            this.borderColor = "Черный";
+        }
+
+        public double getPerimeter() {
+            return 2 * (width + height);
+        }
+
+        public double getArea() {
+            return width * height;
+        }
+
+        public String getFillColor() {
+            return fillColor;
+        }
+
+        public String getBorderColor() {
+            return borderColor;
+        }
+    }
+
+    static class Triangle implements Figure {
+        private double a;
+        private double b;
+        private double c;
+        private String fillColor;
+        private String borderColor;
+
+        public Triangle(double a, double b, double c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.fillColor = "Зеленый";
+            this.borderColor = "Черный";
+        }
+
+        public double getPerimeter() {
+            return a + b + c;
+        }
+
+        public double getArea() {
+            double p = getPerimeter() / 2;
+            return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        public String getFillColor() {
+            return fillColor;
+        }
+
+        public String getBorderColor() {
+            return borderColor;
+        }
+    }
 
     public static void main(String[] args) {
 
@@ -212,6 +309,29 @@ public class Main {
 
         // 2 задание:
 
+        Figure circle = new Circle(2);
+        Figure rectangle = new Rectangle(4, 6);
+        Figure triangle = new Triangle(3, 4, 5);
 
+
+        System.out.println("\nКруг:" +
+                "\nПериметр: " + circle.getPerimeter() +
+                "\nПлощадь: " + circle.getArea() +
+                "\nЦвет заливки: " + circle.getFillColor() +
+                "\nЦвет границы: " + circle.getBorderColor());
+
+
+        System.out.println("\nПрямоугольник:" +
+                "\nПериметр: " + rectangle.getPerimeter() +
+                "\nПлощадь: " + rectangle.getArea() +
+                "\nЦвет заливки: " + rectangle.getFillColor() +
+                "\nЦвет границы: " + rectangle.getBorderColor());
+
+
+        System.out.println("\nТреугольник:" +
+                "\nПериметр: " + triangle.getPerimeter() +
+                "\nПлощадь: " + triangle.getArea() +
+                "\nЦвет заливки: " + triangle.getFillColor() +
+                "\nЦвет границы: " + triangle.getBorderColor());
     }
 }
