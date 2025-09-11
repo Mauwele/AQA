dbd4bb845c78c0677c8220f8e7232e64b24dfc8b
Максим <maksimka.baranov.80@mail.ru>
2025-09-11 12:54:48 +0300

    Fixed errors

--- src/main/java/Lesson_3/Main.java
+++ src/main/java/Lesson_3/Main.java
@@ -2,123 +2,55 @@ package Lesson_3;
 
 public class Main {
 
-    /* 1. Создать класс "Товар" с полями: название, дата производства, производитель, страна происхождения, цена, состояние бронирования покупателем.
-Конструктор класса должен заполнять эти поля при создании объекта.
-Внутри класса «Товар» написать метод, который выводит информацию об объекте в консоль.  /*
-     */
-
-    public static class Product {
-        private String name;
-        private String productionDate;
-        private String manufacturer;
-        private String сountryOfOrigin;
-        private int price;
-        private Boolean statusBooking;
-        public Product( String name, String productionDate, String manufacturer, String сountryOfOrigin, int price, Boolean statusBooking) {
-            this.name = name;
-            this.productionDate = productionDate;
-            this.manufacturer = manufacturer;
-            this.сountryOfOrigin = сountryOfOrigin;
-            this.price = price;
-            this.statusBooking = statusBooking;
-        }
-        public static void ProductInfo(Product product) {
-            System.out.println("\nНазвание: " + product.name
-                    + "\nДата производства: " + product.productionDate
-                    + "\nПроизводитель: " + product.manufacturer
-                    + "\nСтрана происхождения: " + product.сountryOfOrigin
-                    + "\nЦена: " + product.price + " $"
-                    + "\nСостояние бронирования покупателем: " + product.statusBooking);
-        }
-    }
-
-    // 3. Создать класс Park с внутренним классом, с помощью объектов которого можно хранить информацию об аттракционах, времени их работы и стоимости.
-
-    public static class Park {
-        public static class Attraction {
-            private String nameOfAttraction;
-            private String workingTime;
-            private int price;
-
-            public Attraction (String nameOfAttraction, String workingTime, int price) {
-                this.nameOfAttraction = nameOfAttraction;
-                this.workingTime = workingTime;
-                this.price = price;
-            }
-
-            public String getnameOfAttraction() {
-                return nameOfAttraction;
-            }
-
-            public void setNameOfAttraction(String nameOfAttraction) {
-                this.nameOfAttraction = nameOfAttraction;
-            }
-
-            public String getworkingTime(String workingTime) {
-                return workingTime;
-            }
-
-            public void setworkingTime(String workingTime) {
-                this.workingTime = workingTime;
-            }
-
-            public int getprice() {
-                return price;
-            }
-
-            public void setprice(int price) {
-                this.price = price;
-            }
-
-            public void showInfo() { System.out.println("\nАттракцион: " + nameOfAttraction
-                    + "\nВремя работы: " + workingTime
-                    + "\nСтоимость: " + price + " руб.");
-            }
-        }
-    }
-
     public static void main(String[] args) {
 
         // Запрос 1 пункта задания:
-        Product nothingPhone2 = new Product("Nothing Phone 2", "11.07.2023", "Nothing Corp.", "United Kingdom", 653, false);
-        Product.ProductInfo (nothingPhone2);
+        Product nothingPhone2 = new Product("Nothing Phone 2", 11, 7, 2023, "Nothing Corp.", "United Kingdom", 653, false);
+        nothingPhone2.showProductInfo();
 
         // Запрос 2 пункта задания:
         Product[] productsArray = new Product[5];
-        productsArray[0] = new Product("Samsung S25 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 5599, true);
 
-        productsArray[1] = new Product("iPhone 16 Pro Max", "15.03.2025", "Apple Inc.", "China", 11999, false);
+        productsArray[0] = new Product("Samsung S25 Ultra", 1, 2, 2025, "Samsung Corp.", "Korea", 5599, true);
+        productsArray[1] = new Product("iPhone 16 Pro Max", 15, 3, 2025, "Apple Inc.", "China", 11999, false);
+        productsArray[2] = new Product("Xiaomi 15 Ultra", 20, 4, 2025, "Xiaomi Corporation", "China", 899, true);
+        productsArray[3] = new Product("Google Pixel 9 Pro", 10, 5, 2025, "Google LLC", "Vietnam", 999, false);
+        productsArray[4] = new Product("OnePlus 12T", 1, 6, 2025, "OnePlus Technologies", "China", 799, true);
 
-        productsArray[2] = new Product("Xiaomi 15 Ultra", "20.04.2025", "Xiaomi Corporation", "China", 899, true);
-
-        productsArray[3] = new Product("Google Pixel 9 Pro", "10.05.2025", "Google LLC", "Vietnam", 999, false);
-
-        productsArray[4] = new Product("OnePlus 12T", "01.06.2025", "OnePlus Technologies", "China", 799, true);
-
-        Product.ProductInfo (productsArray[2]);
+        productsArray[2].showProductInfo();
 
         //Запрос 3 пункта задания:
-        Park BestPark = new Park();
+        Park bestPark = new Park();
 
-        Park.Attraction rollerCoaster = new Park.Attraction("Американские горки", "09:00-21:00", 300);
+        bestPark.addAttraction("Американские горки", "09:00-21:00", 300);
+        bestPark.addAttraction("Колесо обозрения", "08:00-22:00", 100);
+        bestPark.addAttraction("Карусель", "09:00-21:00", 150);
 
-        Park.Attraction ferrisWheel = new Park.Attraction("Колесо обозрения", "08:00-22:00", 100);
+        System.out.println("\nИнформация об аттракционах:");
+        bestPark.showAllAttractions();
 
-        Park.Attraction carousel = new Park.Attraction("Карусель", "09:00-21:00", 150);
-        
-        rollerCoaster.showInfo();
-        ferrisWheel.showInfo();
-        carousel.showInfo();
+        Object rollerCoaster = bestPark.findAttraction("Американские горки");
+        Object ferrisWheel = bestPark.findAttraction("Колесо обозрения");
+        Object carousel = bestPark.findAttraction("Карусель");
 
-        // Проверка сеттеров
-        rollerCoaster.setprice(500);
-        ferrisWheel.setNameOfAttraction("Большое колесо обозрения");
-        carousel.setworkingTime("08:00-22:00");
+        // Проверка сеттеров и геттеров
+        if (rollerCoaster != null) {
+            ((Park.Attraction) rollerCoaster).setPrice(500);
+            System.out.println("\nПроверка цены: " + ((Park.Attraction) rollerCoaster).getPrice());
+        }
 
-        rollerCoaster.showInfo();
-        ferrisWheel.showInfo();
-        carousel.showInfo();
+        if (ferrisWheel != null) {
+            ((Park.Attraction) ferrisWheel).setNameOfAttraction("Большое колесо обозрения");
+            System.out.println("\nПроверка названия: " + ((Park.Attraction) ferrisWheel).getNameOfAttraction());
+        }
 
+        if (carousel != null) {
+            ((Park.Attraction) carousel).setWorkingTime("08:00-22:00");
+            System.out.println("\nПроверка времени работы: " + ((Park.Attraction) carousel).getWorkingTime());
+        }
+
+        System.out.println("\nИнформация после изменений:");
+        bestPark.showAllAttractions();
     }
-    
+
 }
---
+++ src/main/java/Lesson_3/Product.java
@@ -0,0 +1,34 @@
+package Lesson_3;
+
+import java.util.Date;
+
+/* 1. Создать класс "Товар" с полями: название, дата производства, производитель, страна происхождения, цена, состояние бронирования покупателем.
+Конструктор класса должен заполнять эти поля при создании объекта.
+Внутри класса «Товар» написать метод, который выводит информацию об объекте в консоль.  */
+
+public class Product {
+    private String name;
+    private Date productionDate;
+    private String manufacturer;
+    private String countryOfOrigin;
+    private double price;
+    private boolean statusBooking;
+
+    public Product(String name, int day, int month, int year, String manufacturer, String countryOfOrigin, double price, boolean statusBooking) {
+        this.name = name;
+        this.productionDate = new Date(year - 1900, month - 1, day);
+        this.manufacturer = manufacturer;
+        this.countryOfOrigin = countryOfOrigin;
+        this.price = price;
+        this.statusBooking = statusBooking;
+    }
+
+    public void showProductInfo() {
+        System.out.println("\nНазвание: " + name);
+        System.out.println("Дата производства: " + productionDate.toString());
+        System.out.println("Производитель: " + manufacturer);
+        System.out.println("Страна происхождения: " + countryOfOrigin);
+        System.out.println("Цена: " + price + " $");
+        System.out.println("Состояние бронирования: " + statusBooking);
+    }
+}
