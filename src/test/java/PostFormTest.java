ef94e407fd767b1b3b6c05db18a0b67e084b4cda
Mauwele <maksimka.baranov.80@mail.ru>
2025-09-26 15:36:31 +0300

    Add answer

--- src/test/java/PostFormTest.java
+++ src/test/java/PostFormTest.java
@@ -1,27 +1,20 @@
-import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import org.junit.jupiter.api.Test;
 
 public class PostFormTest {
     @Test
     public void testPostFormData() {
-        try {
-            given()
-                    .baseUri("https://postman-echo.com")
-                    .contentType("application/x-www-form-urlencoded")
-                    .formParam("foo1", "bar1")
-                    .formParam("foo2", "bar2")
-                    .log().all()
-                    .when()
-                    .post("/post")
-                    .then()
-                    .statusCode(200)
-                    .body("form.foo1", equalTo("bar1"))
-                    .body("form.foo2", equalTo("bar2"));
-        } catch (AssertionError e) {
-            System.out.println("Произошла ошибка: " + e.getMessage());
-            // Здесь можно добавить дополнительную обработку
-        }
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .formParam("foo1", "bar1")
+                .formParam("foo2", "bar2")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("form.foo1", equalTo("bar1"))
+                .body("form.foo2", equalTo("bar2"));
     }
 }
